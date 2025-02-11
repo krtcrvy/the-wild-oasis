df7ecdbd2ad640089e3adf77881c61bd88f47779
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-02-12 04:20:10 +0800

    add react-query and eslint-plugin-query for improved data fetching and linting

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter, Route, Routes } from "react-router";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>App</div>} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>App</div>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
