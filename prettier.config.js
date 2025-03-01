1bd9bc9f9810f9812ee466005885da8830b2e87d
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-03-01 08:02:29 +0800

    add Prettier and ESLint configuration for code formatting and linting

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/?/?([a-zA-Z/]*))$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+};
+
+export default config;
