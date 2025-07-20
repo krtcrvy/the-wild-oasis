8e940a8f1d8e432f84b5ecebd6ac99c95a17710c
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2025-07-21 04:12:54 +0800

    init: set up React project with TypeScript, Vite, and Tailwind CSS configuration

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import checkFile from "eslint-plugin-check-file";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "off",
+        { allowConstantExport: true },
+      ],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**": "NEXT_JS_APP_ROUTER_CASE",
+        },
+      ],
+    },
+  },
+]);
